599b55e9c2eac93b8ed86f363488088488a4cdc6
davidbonn <davidbonn@mac.com>
2026-09-14 13:03:11 -0700

    initial commit

---
+++ examples/db.py
@@ -0,0 +1,11 @@
+
+
+import sqlite3
+
+
+class DB:
+    def __init__(self, file:str):
+        pass
+
+    def note_success(self, label: str, src: str, temperature: float, sequence: int):
+        pass
